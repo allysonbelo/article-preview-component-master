570b36a58e17375130c3c542755e8108119e42f2
Allyson <46270902+allysonbelo@users.noreply.github.com>
2021-11-15 10:15:05 -0300

    :sparkles: Add javascript file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const icon = document.querySelector(".icon")
+icon.addEventListener("click", clicou)
+
+function clicou(){
+    document.querySelector(".profile").classList.toggle("active")
+}
+
+
+// icon.addEventListener("mouseover", passouMouse)
+// icon.addEventListener("mouseout", tirouMouse)
+
+// function passouMouse(){
+//     console.log("Passou o mouse");
+// }
+
+// function tirouMouse(){
+//     console.log("Tirou o mouse");
+// }
+
